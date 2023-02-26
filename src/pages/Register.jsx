4f81c31413b006c9f86fa74f82f1df7628866e13
YuukiYamano <yuukiyamano1108@gmail.com>
2023-02-26 11:41:17 +0900

    feat: 登録ページ

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Add from "../img/add_a_photo_FILL0_wght400_GRAD0_opsz48.png"
 
 const Register = () => {
   return (
@@ -10,7 +11,11 @@ const Register = () => {
                 <input type="text" placeholder='display name'/>
                 <input type="email" placeholder='email' />
                 <input type="password" placeholder='password' />
-                <input type="file" />   
+                <input type="file" id='file'/>   
+                <label htmlFor="file ">
+                    <img src={Add} alt="" />
+                    <span>Add an avatar</span>
+                </label>
                 <button>Sign up</button>
             </form>
             <p>You do have an account? <a href="">Login</a></p>
