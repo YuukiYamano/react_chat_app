48ced349b7c110b45aae14300d2b5d79ee8e59b1
YuukiYamano <yuukiyamano1108@gmail.com>
2023-02-26 11:09:49 +0900

    feat: 登録画面の項目追加

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='formContainer'>
+        <div className="formWrapper">
+            <span className="logo">React Chat App</span>
+            <span className="title">Register</span>
+            <form action="">
+                <input type="text" placeholder='display name'/>
+                <input type="email" placeholder='email' />
+                <input type="password" placeholder='password' />
+                <input type="file" />   
+                <button>Sign up</button>
+            </form>
+            <p>You do have an account? <a href="">Login</a></p>
+        </div>
+    </div>
   )
 }
 
